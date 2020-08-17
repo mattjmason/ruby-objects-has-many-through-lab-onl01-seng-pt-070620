8a7db18b81c6db18656252805c6b430a366e1ca1
Matt Mason <masonmj10@gmail.com>
2020-08-17 05:43:07 +0000

    Done.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,9 +1,16 @@
 class Doctor 
+<<<<<<< HEAD
   
 attr_accessor :name
   @@all = []
 
   def initialize(name)
+=======
+  attr_accessor :name 
+  @@all = []
+  
+ def initialize(name)
+>>>>>>> d331ce9e606083950001304959cb8c16461607ac
     @name = name
     @@all << self
   end
@@ -13,17 +20,26 @@ attr_accessor :name
   end
 
   def patients
+<<<<<<< HEAD
     appointments.collect do |appointment| 
       appointment.patient
   end 
+=======
+    appointments.map {|appointment| appointment.patient}
+>>>>>>> d331ce9e606083950001304959cb8c16461607ac
   end
 
   def self.all
     @@all
   end
 
+<<<<<<< HEAD
   def new_appointment(date, patient)
     Appointment.new(date, patient, self)
+=======
+  def new_appointment(patient, date)
+    Appointment.new(patient, date, self)
+>>>>>>> d331ce9e606083950001304959cb8c16461607ac
   end
   
 end 
